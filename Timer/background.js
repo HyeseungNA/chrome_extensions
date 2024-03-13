fd8d0a67b328b4c8e93c487be6da67c50ef7a92f
HyeseungNA <nahs00122@gmail.com>
2024-03-14 08:55:47 +0900

    append child div in contentscript

--- Timer/background.js
+++ Timer/background.js
@@ -58,18 +58,15 @@
 //   });
 // });
 
-function myFunction() {
-  alert("안녕");
-}
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  // 크롬 api에 접근하기 위해 background.js에 작성
   if (changeInfo.status === "complete" && tab.url.includes("problem/")) {
     console.log(tab);
     tabId = tabId;
     chrome.scripting
       .executeScript({
         target: { tabId: tabId },
-        func: myFunction,
+        files: ["content-script.js"],
       })
       .then(() => {
         console.log("에에에");
--- Timer/content-script.js
+++ Timer/content-script.js
@@ -1,29 +1,16 @@
-// let timerContainer = document.createElement("div");
-// // CSS 스타일 적용
-// timerContainer.style.width = "200px"; // 너비 설정
-// timerContainer.style.height = "100px"; // 높이 설정
-// timerContainer.style.backgroundColor = "lightblue"; // 배경색 설정
-// document.body.appendChild(timerContainer);
+function myFunction() {
+  let redSquare = document.createElement("div");
+  redSquare.style.width = "100px";
+  redSquare.style.height = "100px";
+  redSquare.style.backgroundColor = "red";
+  redSquare.style.position = "absolute";
+  redSquare.style.top = 0;
+  redSquare.style.right = 0;
 
-// const spanElemnets = document.querySelectorAll("span");
+  let container = document.querySelector("wrapper.container.content");
+  container.append(redSquare);
+}
 
-// alert(spanElemnets);
+myFunction();
 
-window.addEventListener("DOMContentLoaded", () => {
-  // 현재 페이지의 URL을 가져옴
-  const currentURL = window.location.href;
-
-  // URL에 "problem"이 포함되어 있는지 확인
-  if (currentURL.includes("problem")) {
-    // "problem"이 포함되어 있으면 alert 띄우기
-    alert("이 페이지는 문제 페이지입니다!");
-  }
-});
-
-// window.addEventListener("DO MContentLoaded", () => {
-//   // 페이지의 div.wrapper 요소를 선택하여 배경색을 변경
-//   const wrapper = document.querySelector("div.wrapper");
-//   if (wrapper) {
-//     wrapper.style.backgroundColor = "red";
-//   }
-// });
+console.log("하이");
